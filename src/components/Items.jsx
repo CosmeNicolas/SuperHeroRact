3b1d210808238eb19b5061972855d375dbefba96
CosmeNicolas <nicomas7590@gmail.com>
2024-01-23 17:41:55 -0300

    agregue sistema de grillas en el componente items

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,4 +1,4 @@
-import { Button, Card } from "react-bootstrap";
+import {  Card , Col, Row, Container  } from "react-bootstrap";
 
 const Items = ({ datoHeroe }) => {
   /* if(!datoHeroe){
@@ -7,20 +7,26 @@ const Items = ({ datoHeroe }) => {
   } */
   return (
     <>
-      {
-        datoHeroe.map((itemHeroe, idHeroe) => (
-          <Card key={idHeroe}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+    <Container >
+      <Row  xs={1} md={2} xl={4}>
+      {datoHeroe.map((itemHeroe, idHeroe) => {
+         return (
+          <Col  key={idHeroe}  >
+          <Card className="card-comic mt-2">
+          <Card.Img variant="top" src={`${itemHeroe.thumbnail.path}.${itemHeroe.thumbnail.extension}`} fluid />
             <Card.Body>
               <Card.Title>{itemHeroe.title}</Card.Title>
               <Card.Text>
                 Aqui va el texto
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+            {/*   <Button variant="primary">Go somewhere</Button> */}
             </Card.Body>
           </Card>
-        ))
-      }
+          </Col>
+        )
+      })}
+      </Row>
+    </Container>
     </>
   );
 };
