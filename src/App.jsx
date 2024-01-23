89c544086347649f727e126796d27457ed936a67
CosmeNicolas <nicomas7590@gmail.com>
2024-01-23 11:36:38 -0300

    agregue un useState para tomar los textos que ingresan a traves del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,7 @@ import Footer from "./components/Footer"
 import Formulario from "./components/Formulario"
 
 function App() {
-  const [BuscarHeroe, setBuscarHeroe] = useState('')
-
-
-
-  const publicKey = '5cab833b6006af603f7691db641a2373';
-
-  const MostrarApi = async ()=>{
-    try {
-      const ts = 2000
-      const hash = (`1b77f26bd6206b4e08e30562e6d1f39b`)
-      const respuestaApiHero = await fetch(`
-      http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`)
-      const datos  = await respuestaApiHero.json()
-      console.log(datos)
-    } catch (error) {
-     console.error('no hay respuesta')
-    }
-
-  }
-  MostrarApi()
+ 
 
 
   return (
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,41 @@
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap"
+useState
 
 
 const Formulario = () => {
+    const [BuscarHeroe, setBuscarHeroe] = useState('')
+    console.log(' desde el input ')
 
- 
+/* Consumo de API  */
+    const publicKey = '5cab833b6006af603f7691db641a2373';
+    const MostrarApi = async ()=>{
+      try {
+        const ts = 2000
+        const hash = (`1b77f26bd6206b4e08e30562e6d1f39b`)
+        const respuestaApiHero = await fetch(`
+        http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`)
+        const datos  = await respuestaApiHero.json()
+        console.log(datos)
+      } catch (error) {
+       console.error('no hay respuesta')
+      }
+  
+    }
+    MostrarApi()
+ /* Termina consumo de API  */
 
   return (
 
        <Form className="d-flex flex-row align-items-center justify-content-center">
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className="text-light">Buscar SuperHeroe</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+         type="text" 
+         placeholder="Buscar SuperHeroe" 
+         value={BuscarHeroe}
+         onChange={(e)=>setBuscarHeroe(e.target.value)}/>
+          
       </Form.Group>
      
       <Button className="mt-3 ms-1" variant="primary" type="submit">
