b8e9ffbeada0f0020fcbb2494fc2d94b538d887e
CosmeNicolas <nicomas7590@gmail.com>
2024-01-24 13:50:27 -0300

    agregue estilo al boton y al titulo del producto comic , para q se vea de manera transparente

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -9,16 +9,16 @@ const Items = ({ datoHeroe }) => {
       {datoHeroe.map((itemHeroe) => {
          return (
           <Col className="my-2"  key={itemHeroe.id}  >
-          <Card className=" bg bg-dark h-100 position-relativ">
+          <Card className=" bg bg-dark h-100">
           <Card.Img className="img-inCard"  variant="top" src={`${itemHeroe.thumbnail.path}.${itemHeroe.thumbnail.extension}`}  />
           <Card.ImgOverlay className="img-overlay d-flex align-items-center justify-content-center ">
-                <Card.Title className="card-title text-gray text-center bg bg-black rounded-3 opacity-50 ">{itemHeroe.title}</Card.Title>
+                <Card.Title className="card-title text-light rounded-2 opacity-50 bg bg-dark py-2">{itemHeroe.title}</Card.Title>
             </Card.ImgOverlay>
           <ListGroup className="bg bg-black" variant="flush">
             <ListGroup.Item className="bg bg-dark text-light">Precio: ${itemHeroe.prices[0].price} </ListGroup.Item>
             <ListGroup.Item className="bg bg-dark text-light">Format: {itemHeroe.format}  </ListGroup.Item>
           </ListGroup>
-          <div className="py-2 position-absolute bottom-0 w-100">
+          <div className="py-2 position-relative bottom-0 w-100">
           <Card.Body className="bg bg-dark text-center" > 
              <Button className="fuente d-flex flex-column align-items-center" href={itemHeroe.urls[0].url} variant="danger">Ver Mas</Button> 
           </Card.Body>
