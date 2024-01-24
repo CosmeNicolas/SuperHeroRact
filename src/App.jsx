57bf98f627d5aa1fd65dd354e4a791585038f1ac
CosmeNicolas <nicomas7590@gmail.com>
2024-01-24 11:19:43 -0300

    agregue la funcion de montaje con useEffect , para q la apgina no este vacia al momento de hacerl el primer montaje , me muestre algunos comics

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 import {Container} from 'react-bootstrap'
-import { useState } from "react"
 import Footer from "./components/Footer"
 import Formulario from "./components/Formulario"
 import Imagen from "./components/Imagen"
 
 
 function App() {
-  const [BuscarHeroe, setBuscarHeroe] = useState('')
-  const [datoHeroe, setDatoHeroe] =useState([])
-
-    /* Consumo de API  */
-    const publicKey = '5cab833b6006af603f7691db641a2373';
-    const MostrarApi = async (buscarComic)=>{
-      try {
-        const ts = 2000
-        const hash = (`1b77f26bd6206b4e08e30562e6d1f39b`)
-        const respuestaApiHero = await fetch(`
-        https://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}&titleStartsWith=${buscarComic}`)
-        const datos  = await respuestaApiHero.json()
-        /* chequear si todo va bien  */
-        if(respuestaApiHero.ok){
-          console.log(datos.data.results)
-          setDatoHeroe(datos.data.results)
-        }else{
-          console.log('tenes error papurri')
-        }
-        
-        
-        } catch (error) {
-         console.error('no hay respuesta')
-        }
-    
-      }
+  
 
   return (
     <>
      <Container className="main">
       <Imagen/>
       <h1 className="text-light text-center mt-5 fuente">Super Heroe</h1>
-      <Formulario BuscarHeroe={BuscarHeroe}  setBuscarHeroe={setBuscarHeroe}  MostrarApi={MostrarApi} datoHeroe={datoHeroe} />
+      <Formulario  />
      </Container>
      <Footer/>
     </>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Form, Button } from "react-bootstrap"
 useState
 import Items from "./Items";
 
-const Formulario = ({BuscarHeroe,setBuscarHeroe,MostrarApi, datoHeroe}) => {
+const Formulario = () => {
    
+
+  const [BuscarHeroe, setBuscarHeroe] = useState('')
+  const [datoHeroe, setDatoHeroe] =useState([])
+
+    /* Consumo de API  */
+    const publicKey = '5cab833b6006af603f7691db641a2373';
+    const MostrarApi = async (buscarComic)=>{
+      try {
+        const ts = 2000
+        const hash = (`1b77f26bd6206b4e08e30562e6d1f39b`)
+        const respuestaApiHero = await fetch(`
+        https://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}&titleStartsWith=${buscarComic}`)
+        const datos  = await respuestaApiHero.json()
+        /* chequear si todo va bien  */
+        if(respuestaApiHero.ok){
+          console.log(datos.data.results)
+          setDatoHeroe(datos.data.results)
+        }else{
+          console.log('tenes error papurri')
+        }
+        
+        
+        } catch (error) {
+         console.error('no hay respuesta')
+        }
+      }
+/*  llamada de comic al inicio , en montaje  */
+        const montajeComic = async ()=>{
+          try {
+            const ts = 2000
+            const hash = (`1b77f26bd6206b4e08e30562e6d1f39b`)
+            const respuestaApiHero = await fetch(`
+            https://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=6`)
+            const datos  = await respuestaApiHero.json()
+            /* chequear si todo va bien  */
+            if(respuestaApiHero.ok){
+              console.log(datos.data.results)
+              setDatoHeroe(datos.data.results)
+            }else{
+              console.log('tenes error papurri')
+            }
+            } catch (error) {
+             console.error('no hay respuesta facha')
+            }
+          }
+
+        useEffect(() => {
+        montajeComic()
+        }, [])
+
+
     const handleSubmit = (e) => {
         e.preventDefault()
         MostrarApi(BuscarHeroe);
+        setBuscarHeroe('')
         }
 
   return (
@@ -22,6 +74,8 @@ const Formulario = ({BuscarHeroe,setBuscarHeroe,MostrarApi, datoHeroe}) => {
         <Form.Control
          type="text" 
          placeholder="Buscar SuperHeroe" 
+         minLength={3}
+         maxLength={50}
          value={BuscarHeroe}
          onChange={(e)=>setBuscarHeroe(e.target.value)}/>
       </Form.Group>
